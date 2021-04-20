c5281cb05722610cc5fe60576c9859925d242e3f
Fengzi <LHF20061116@126.com>
2021-04-20 15:47:21 -0500

    0.2.0 beta update!
    hopefully can fix all the things

--- main.js
+++ main.js
@@ -8,14 +8,24 @@ var mainWindow = null;
 var tray = null;
 var isQuiting = false;
 
+// Development Mode (Debug / Performance)
+var devMode = true;
+function devMsg(message){
+    if(devMode){
+        console.log(message);
+    }
+}
+devMsg("Initialize Complete.");
+
 app.on("ready", ()=>{
+    devMsg("App Ready.");
     // Start the load window
     var load = new electron.BrowserWindow({
         width: 128,
         height: 128,
-        // title: "正在加载...",
+        title: "正在加载...",
         frame: false,
-        icon: path.join(__dirname, "icon.png"),
+        icon: path.join(__dirname, "winicon.ico"),
         show: false
     });
     load.loadFile(path.join(__dirname, "load.png"));
@@ -23,13 +33,14 @@ app.on("ready", ()=>{
     load.setResizable(false);
     load.on("ready-to-show", ()=>{
         load.show();
-    })
+        devMsg("Load Window Showed.");
+    });
     // Load the main window
     mainWindow = new electron.BrowserWindow({
         width: 1280,
         height: 720,
         show: false,
-        icon: path.join(__dirname, "icon.png")
+        icon: path.join(__dirname, "winicon.ico")
     });
     // Load tray icon
     tray = new electron.Tray(path.join(__dirname, "icon.ico"));
@@ -50,6 +61,10 @@ app.on("ready", ()=>{
             app.relaunch();
             app.exit();
         }},
+        // {label: "听完退出", click: ()=>{
+        //     isQuiting = true;
+        //     app.quit();
+        // }},
         {label: "退出", click: ()=>{
             isQuiting = true;
             app.quit();
@@ -65,6 +80,7 @@ app.on("ready", ()=>{
             mainWindow.show();
         }
     });
+    devMsg("Tray Load Complete.");
     // Set app menu
     var macMenu = new electron.Menu.buildFromTemplate([
         {
@@ -92,6 +108,14 @@ app.on("ready", ()=>{
                 {label: "退出", click: ()=>{
                     isQuiting = true;
                     app.quit();
+                }},
+                {label: "beta版专属macOS终极启动修复程序designed for ZBX", click: ()=>{
+                    devMsg("Main Window Ready To Show.");
+                    load.close();
+                    mainWindow.show();
+                    tray.setToolTip("疯子音乐主进程");
+                    clearTimeout(startupTimeout);
+                    devMsg("Main Window Ready.");
                 }}
             ]
         },
@@ -121,7 +145,7 @@ app.on("ready", ()=>{
                     var doc = new electron.BrowserWindow({
                         width: 1280,
                         height: 720,
-                        icon: path.join(__dirname, "icon.png")
+                        icon: path.join(__dirname, "winicon.ico")
                     });
                     doc.loadURL("https://docs.music.fengzi.ga");
                     doc.setMenu(null);
@@ -130,38 +154,43 @@ app.on("ready", ()=>{
                     var about = new electron.BrowserWindow({
                         width: 720,
                         height: 480,
-                        icon: path.join(__dirname, "icon.png")
+                        icon: path.join(__dirname, "winicon.ico")
                     });
                     about.loadFile(path.join(__dirname, "about.html"));
                     about.setMenu(null);
+                }},
+                {label: "beta版专属macOS终极启动修复程序designed for ZBX", click: ()=>{
+                    devMsg("Force fix started");
+                    load.close();
+                    mainWindow.show();
+                    tray.setToolTip("疯子音乐主进程");
+                    clearTimeout(startupTimeout);
+                    devMsg("Main Window Ready yay!");
                 }}
             ]
         }
     ]);
-    mainWindow.setMenu(macMenu);
-    // electron.Menu.setApplicationMenu(macMenu);
-    // Try to load Discord status
-    var richPresence = require("discord-rich-presence")("826466733451640852");
-    richPresence.updatePresence({
-        state: "music.fengzi.ga",
-        startTimestamp: new Date(),
-        largeImageKey: "logo",
-        largeImageText: "Hi!"
-    });
-    // Really bad to add but it will make the load screen look much better
-    // (Then why do i need a load screen?)
-    // (Who knows...)
-    // (Life is difficult)
-    // (Maybe I'll just call it a logo screen)
-    // (Who doesn't enjoy such a pretty logo?)
-    setTimeout(function(){
-        mainWindow.loadFile(path.join(__dirname, "index.html"));
-    }, 500);
+    // mainWindow.setMenu(macMenu);
+    electron.Menu.setApplicationMenu(macMenu);
+    devMsg("App Menu Load Complete.");
+    mainWindow.loadFile(path.join(__dirname, "index.html"));
+    devMsg("Main Content Loaded.");
+    // Shrug.
+    var startupTimeout = setTimeout(function(){
+        if (!mainWindow.isVisible()){
+            app.relaunch();
+            app.exit();
+        }
+    }, 60000);
     mainWindow.on("ready-to-show", ()=>{
+        devMsg("Main Window Ready To Show.");
         load.close();
         mainWindow.show();
         tray.setToolTip("疯子音乐主进程");
-        // mainWindow.focus();
+        // Maybe a fix for stucking background color at start, but idk.
+        mainWindow.focus();
+        clearTimeout(startupTimeout);
+        devMsg("Main Window Ready.");
     });
     // Hide window and continue playing instead of closing
     mainWindow.on("close", (e)=>{
@@ -174,7 +203,7 @@ app.on("ready", ()=>{
         var oof = new electron.BrowserWindow({
             width: 640,
             height: 480,
-            icon: path.join(__dirname, "icon.png")
+            icon: path.join(__dirname, "winicon.ico")
         });
         oof.loadFile(path.join(__dirname, "oof.html"));
         oof.setMenu(null);
@@ -182,6 +211,15 @@ app.on("ready", ()=>{
     mainWindow.on("responsive", ()=>{
         oof.close();
     });
+    // Try to load Discord status
+    var richPresence = require("discord-rich-presence")("826466733451640852");
+    richPresence.updatePresence({
+        state: "music.fengzi.ga",
+        startTimestamp: new Date(),
+        largeImageKey: "logo",
+        largeImageText: "Hi!"
+    });
+    devMsg("Discord Status Module Load Complete.");
 });
 // Allow manually quit
 app.on("before-quit", function(){
